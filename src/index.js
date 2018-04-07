5d3856ac90eebeaa46ef9d3299aa8c4fa8ab8e08
Alvin <alvin.ghouas@gmail.com>
2018-04-07 11:02:36 -0700

    Create Color blocks on key press

--- src/index.js
+++ src/index.js
@@ -1,31 +1,84 @@
 import './index.css';
 import * as d3 from 'd3';
-import {getNumbers} from './api/numbersApi';
+/**
+ * Init
+ */
+const key = (d) => d.key;
+let id = 2;
 
-const width = 420,barHeight = 20;
+//Create colors
+let colors = []
 
-getNumbers().then(numbers => {
-  const data = numbers.map(n => parseInt(n));
+for (let i = 0; i < 26; i++) {
+  colors.push('#'+Math.floor(Math.random()*16777215).toString(16));
+}
 
-  const x = d3.scaleLinear()
-    .domain([0, d3.max(data)])
-    .range([0, width]);
+const mapColorsToAlphabet = (colors) => {
+  const map = new Map();
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  for (let i = 0; i < alphabet.length; i++) {
+    map.set(alphabet[i], colors[i])
+  }
+  return map;
+}
 
-  const chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", barHeight * data.length);
-  const bar = chart.selectAll("g")
-    .data(data)
-    .enter().append("g")
-    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+const colorMap = mapColorsToAlphabet(colors);
 
-  bar.append("rect")
-    .attr("width", x)
-    .attr("height", barHeight - 1);
+let dataset = [];
+/*const yScale = d3.scaleLinear()
+  .domain([0, d3.max(dataset, (d) => d.value)])
+  .range([0, h]);*/
 
-  bar.append("text")
-    .attr("x", function(d) { return x(d) - 3; })
-    .attr("y", barHeight / 2)
-    .attr("dy", ".35em")
-    .text(function(d) { return d; });
+/*  const svg = d3.select("svg");
+
+  svg.attr("width", width)
+  .attr("height", height) */
+
+  const colorBlocks = d3.select("body").append("ul").attr("class", "color-blocks");
+
+  colorBlocks.selectAll("li")
+  .data(dataset, key)
+  .enter()
+  .append("li")
+  .attr("class", "color-block")
+  .style("background-color", function(d) {
+  return colorMap.get(d.value);
+  })
+
+d3.select("body").on("keydown", function(){
+    const keyCode = d3.event.keyCode
+    if(keyCode != 8){
+      const newChar = String.fromCharCode(d3.event.keyCode).toLowerCase();
+      dataset.push({key: id++, value: newChar});
+
+      const colorBlocks = d3.selectAll("li").data(dataset,key);
+      colorBlocks.enter()
+      .append("li")
+      .attr("class", "color-block")
+      .style("background-color", function(d) {
+        return colorMap.get(d.value);
+      })
+      .style("opacity", 0)
+      .transition()
+      .duration(200)
+      .style("opacity", 1);
+
+    }else{
+      dataset.splice(-1,1);
+      const colorBlocks = d3.selectAll("li").data(dataset,key);
+      colorBlocks.exit()
+      .transition()
+      .duration(100)
+      .style("opacity", 0)
+      .remove()
+
+    }
+
+  });
+
+d3.select("input").on("change", function(){
+  const input = document.getElementById('show-letters');
 });
+
+
+
