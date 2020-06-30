039834819c882ccbcfabbd9d36de5db23dfcc70f
Alvin <alvin.ghouas@gmail.com>
2020-06-30 09:08:21 -0700

    updates

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ let dataset = [];
   svg.attr("width", width)
   .attr("height", height) */
 
-  const colorBlocks = d3.select("body").append("ul").attr("class", "color-blocks");
+  const colorBlocks = d3.select(".container").append("ul").attr("class", "color-blocks");
 
   colorBlocks.selectAll("li")
   .data(dataset, key)
@@ -44,41 +44,81 @@ let dataset = [];
   .style("background-color", function(d) {
   return colorMap.get(d.value);
   })
+  .text((d)=> d.value)
+  .classed("hide-text", true);
 
 d3.select("body").on("keydown", function(){
     const keyCode = d3.event.keyCode
     if(keyCode != 8){
-      const newChar = String.fromCharCode(d3.event.keyCode).toLowerCase();
-      dataset.push({key: id++, value: newChar});
+      if(isValidLetter(keyCode)){
+        const newChar = String.fromCharCode(d3.event.keyCode).toLowerCase();
+        dataset.push({key: id++, value: newChar});
 
-      const colorBlocks = d3.selectAll("li").data(dataset,key);
-      colorBlocks.enter()
-      .append("li")
-      .attr("class", "color-block")
-      .style("background-color", function(d) {
-        return colorMap.get(d.value);
-      })
-      .style("opacity", 0)
-      .transition()
-      .duration(200)
-      .style("opacity", 1);
+        const colorBlocks = d3.select("ul").selectAll("li").data(dataset,key);
+        colorBlocks.enter()
+        .append("li")
+        .text((d)=>d.value)
+        .attr("class", "color-block hide-text")
+        .style("background-color", function(d) {
+          return colorMap.get(d.value);
+        })
+        .style("opacity", 0)
+        .transition()
+        .duration(200)
+        .style("opacity", 1)
 
+        handleToggleEvent();
+      }
     }else{
-      dataset.splice(-1,1);
-      const colorBlocks = d3.selectAll("li").data(dataset,key);
-      colorBlocks.exit()
-      .transition()
-      .duration(100)
-      .style("opacity", 0)
-      .remove()
+        dataset.splice(-1,1);
+        const colorBlocks = d3.select("ul").selectAll("li").data(dataset,key);
+        colorBlocks.exit()
+        .transition()
+        .duration(100)
+        .style("opacity", 0)
+        .remove()
 
+        handleToggleEvent();
     }
 
   });
 
-d3.select("input").on("change", function(){
-  const input = document.getElementById('show-letters');
-});
+  const isValidLetter =(keyCode) =>{
+    if( (keyCode>= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122 || keyCode === 32)){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+const handleToggleEvent =() =>{
+  const showText = d3.select("input").node().checked;
+  if(showText){
+    //re draw all the elements showing text
+    const colorBlocks = d3.selectAll('li');
+    colorBlocks.each(function(d, i) {
+      d3.select(this)
+        .classed("hide-text", false)
+        .classed("show-text", true);
+    });
+  }else{
+    const colorBlocks = d3.selectAll('li');
+    colorBlocks.each(function(d, i) {
+      d3.select(this)
+      .classed("show-text", false)
+      .classed("hide-text", true);
+    });
+  }
+
+};
+d3.select("input").on("change", handleToggleEvent);
+
+
+//TODO: add drop down for dataset selection
+//TODO: toggle between text and color blocks
+//chooseDataset()
+//update()
+//showText global state.
 
 
 
